ce8fe8d58860db27bf915a6f48834af101274d9b
Speshultee <61945242+Speshultee@users.noreply.github.com>
2021-07-11 02:50:07 -0500

    New - $codeblock
    Puts the content in the brackets into a codeblock.
    
    $codeblock[input]
    $codeblock[input;syntax]
    
    input - The content to put into a codeblock
    syntax - Optional syntax highlighting (only seen by pc users)

---
+++ package/functions/codeblock.js
@@ -0,0 +1,19 @@
+const { docs } = require("../functions/docs/docs.json");
+
+const codeblock = async (client, message, args, name, code) => {
+
+    const r = code.split("$codeblock[").length - 1
+    let inside = code.split("$codeblock[")[r].split("]")[0]
+
+    let [input, syntax = ""] = inside.split(";");
+
+    if (!input) return message.channel.send(`:x: Missing content in \`$codeblock[${inside}]\`.\n${docs.action}/codeblock`)
+
+    code = code.replaceLast(`$codeblock[${inside}]`, `\`\`\`${syntax}\n${input}\n\`\`\``)
+
+    return {
+        code: code
+    }
+}
+
+module.exports = codeblock;
