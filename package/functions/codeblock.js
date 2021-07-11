4ce78a8e6063d6c7e6375bf75692459f45296b89
Speshultee <61945242+Speshultee@users.noreply.github.com>
2021-07-11 04:04:26 -0500

    Updated - $codeblock
    Just define 2nd field as yes of true to return the codeblocked content with JavaScript syntax highlighting.

--- package/functions/codeblock.js
+++ package/functions/codeblock.js
@@ -5,9 +5,16 @@ const codeblock = async (client, message, args, name, code) => {
     const r = code.split("$codeblock[").length - 1
     let inside = code.split("$codeblock[")[r].split("]")[0]
 
-    let [input, syntax = ""] = inside.split(";");
+    let [input, syntax] = inside.split(";");
 
     if (!input) return message.channel.send(`:x: Missing content in \`$codeblock[${inside}]\`.\n${docs.action}/codeblock`)
+
+
+    if (syntax === "yes" || syntax === "true") {
+        syntax = "js"
+    } else {
+        syntax = ""
+    }
 
     code = code.replaceLast(`$codeblock[${inside}]`, `\`\`\`${syntax}\n${input}\n\`\`\``)
 
