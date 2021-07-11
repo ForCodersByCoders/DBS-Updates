ca5fa17ee48f17698e3c239665af27e930a6f8bd
Speshultee <61945242+Speshultee@users.noreply.github.com>
2021-07-11 00:54:48 -0500

    $guildChannels - Added separator in 3rd field

--- package/functions/guildChannels.js
+++ package/functions/guildChannels.js
@@ -10,19 +10,24 @@ const guildChannels = async (client, message, args, name, code) => {
 
         let [
             guildID,
-            option = "name"
+            option = "name",
+            separator
         ] = inside.split(";");
 
         let guild = client.guilds.cache.get(guildID ? guildID : message.guild.id)
 
         if (!guild) return message.channel.send(`:x: Invalid guild ID in \`$guildChannels[${inside}]\`.\n${docs.data}/guildchannels`)
 
+
+        if (!separator) separator = ", "
+
+
         if (option === "mention") {
-            code = code.replaceLast(`$guildChannels[${inside}]`, guild.channels.cache.filter(channel => (channel.id !== message.guild.id) && (channel.type !== "category")).map(channel => `<#${channel.id}>`).join(", "))
+            code = code.replaceLast(`$guildChannels[${inside}]`, guild.channels.cache.filter(channel => (channel.id !== message.guild.id) && (channel.type !== "category")).map(channel => `<#${channel.id}>`).join(separator))
         } else if (option === "id") {
-            code = code.replaceLast(`$guildChannels[${inside}]`, guild.channels.cache.filter(channel => (channel.id !== message.guild.id) && (channel.type !== "category")).map(channel => channel.id).join(", "))
+            code = code.replaceLast(`$guildChannels[${inside}]`, guild.channels.cache.filter(channel => (channel.id !== message.guild.id) && (channel.type !== "category")).map(channel => channel.id).join(separator))
         } else {
-            code = code.replaceLast(`$guildChannels[${inside}]`, guild.channels.cache.filter(channel => (channel.id !== message.guild.id) && (channel.type !== "category")).map(channel => channel.name).join(", "))
+            code = code.replaceLast(`$guildChannels[${inside}]`, guild.channels.cache.filter(channel => (channel.id !== message.guild.id) && (channel.type !== "category")).map(channel => channel.name).join(separator))
         }
 
         return {
