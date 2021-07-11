d6b7e4b42404cb9329db88e64fa0666df0ed3219
Speshultee <61945242+Speshultee@users.noreply.github.com>
2021-07-11 00:56:14 -0500

    $guildEmojis- Added separator in 3rd field

--- package/functions/guildEmojis.js
+++ package/functions/guildEmojis.js
@@ -10,19 +10,23 @@ const guildEmojis = async (client, message, args, name, code) => {
 
         let [
             guildID,
-            option = "emoji"
+            option = "emoji",
+            separator
         ] = inside.split(";");
 
         let guild = client.guilds.cache.get(guildID ? guildID : message.guild.id)
 
         if (!guild) return message.channel.send(`:x: Invalid guild ID in \`$guildEmojis[${inside}]\`.\n${docs.data}/guildemojis`)
 
+        if (!separator) separator = ", "
+
+
         if (option === "name") {
-            code = code.replaceLast(`$guildEmojis[${inside}]`, guild.emojis.cache.filter(emoji => emoji.id !== message.guild.id).map(emoji => emoji.name).join(", "))
+            code = code.replaceLast(`$guildEmojis[${inside}]`, guild.emojis.cache.filter(emoji => emoji.id !== message.guild.id).map(emoji => emoji.name).join(separator))
         } else if (option === "id") {
-            code = code.replaceLast(`$guildEmojis[${inside}]`, guild.emojis.cache.filter(emoji => emoji.id !== message.guild.id).map(emoji => emoji.id).join(", "))
+            code = code.replaceLast(`$guildEmojis[${inside}]`, guild.emojis.cache.filter(emoji => emoji.id !== message.guild.id).map(emoji => emoji.id).join(separator))
         } else {
-            code = code.replaceLast(`$guildEmojis[${inside}]`, guild.emojis.cache.filter(emoji => emoji.id !== message.guild.id).map(emoji => emoji.toString()).join(", "))
+            code = code.replaceLast(`$guildEmojis[${inside}]`, guild.emojis.cache.filter(emoji => emoji.id !== message.guild.id).map(emoji => emoji.toString()).join(separator))
         }
 
         return {
