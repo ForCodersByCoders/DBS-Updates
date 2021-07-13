774ac52159b6aa3bf9ec350a0ccec3a5b9417b31
Speshultee <61945242+Speshultee@users.noreply.github.com>
2021-07-12 21:15:31 -0500

    Edited - $abbreviate
    Added abbreviations for even larger numbers.
    Contribution by Harrison#3680

--- package/functions/abbreviate.js
+++ package/functions/abbreviate.js
@@ -6,7 +6,7 @@ const abbreviate = async (client, message, args, name, code) => {
     let inside = code.split("$abbreviate[")[r].split("]")[0]
     let numb = inside;
 
-    if (!numb) return message.channel.send(`:x: no message provided in 2nd field of \`$abbreviate[${inside}]\`.\n${docs.action}/abbreviate`);
+    if (!numb) return message.channel.send(`:x: No number provided in 2nd field of \`$abbreviate[${inside}]\`.\n${docs.action}/abbreviate`);
     if (isNaN(numb)) return message.channel.send(`:x: Invalid number in \`$abbreviate[${inside}]\`.\n${docs.action}/abbreviate`);
 
     const formatCash = n => {
@@ -23,7 +23,17 @@ const abbreviate = async (client, message, args, name, code) => {
         if (n >= 1e30 && n < 1e33) return +(n / 1e30).toFixed(1) + "No"
         if (n >= 1e33 && n < 1e36) return +(n / 1e33).toFixed(1) + "Dc";
         if (n >= 1e36 && n < 1e39) return +(n / 1e36).toFixed(1) + "Ud";
-        if (n >= 1e39) return +(n / 1e39).toFixed(1) + "Dd";
+        if (n >= 1e39 && n < 1e42) return +(n / 1e39).toFixed(1) + "Dd";
+        if (n >= 1e42 && n < 1e45) return +(n / 1e42).toFixed(1) + "Td";
+        if (n >= 1e45 && n < 1e48) return +(n / 1e45).toFixed(1) + "Qad";
+        if (n >= 1e48 && n < 1e51) return +(n / 1e48).toFixed(1) + "Qid";
+        if (n >= 1e51 && n < 1e54) return +(n / 1e51).toFixed(1) + "Sxd";
+        if (n >= 1e54 && n < 1e57) return +(n / 1e54).toFixed(1) + "Spd";
+        if (n >= 1e57 && n < 1e60) return +(n / 1e57).toFixed(1) + "Ocd";
+        if (n >= 1e60 && n < 1e63) return +(n / 1e60).toFixed(1) + "Nod";
+        if (n >= 1e63 && n < 1e66) return +(n / 1e63).toFixed(1) + "Vg";
+        if (n >= 1e66 && n < 1e69) return +(n / 1e66).toFixed(1) + "Uvg";
+        if (n >= 1e69) return +(n / 1e69).toFixed(1) + "Dvg";
     };
 
     return {
