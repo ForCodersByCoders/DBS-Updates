d57ff0674f564cf01c45eefdbce8c24b9480c237
Speshultee <61945242+Speshultee@users.noreply.github.com>
2021-07-14 10:04:49 -0500

    Add files via upload

---
+++ package/functions/lowercaseCount.js
@@ -0,0 +1,19 @@
+const lowercaseCount = async (client, message, args, name, code) => {
+    let r = code.split("$lowercaseCount[").length - 1
+    let inside = code.split("$lowercaseCount[")[r].split("]")[0]
+
+    let result;
+    if (!inside) {
+        result = "undefined"
+    } else {
+        result = inside.match(/[a-z]/g).length
+    }
+
+    code = code.replaceLast(`$lowercaseCount[${inside}]`, result)
+
+    return {
+        code: code
+    }
+}
+
+module.exports = lowercaseCount
