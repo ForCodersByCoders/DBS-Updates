b6ca92949c112e66bc208ae9b6266755706266e0
Speshultee <61945242+Speshultee@users.noreply.github.com>
2021-07-15 20:25:38 -0500

    $isString - Shortened code

--- package/functions/isString.js
+++ package/functions/isString.js
@@ -1,16 +1,10 @@
 const isString = (client, message, args, name, code) => {
+
     let r = code.split("$isString[").length - 1
+
     let inside = code.split("$isString[")[r].split("]")[0]
 
-    let result = undefined
-    if (!isNaN(inside) === true) {
-        result = false
-    }
-    else if (!isNaN(inside) === false) {
-        result = true
-    }
-
-    code = code.replaceLast(`$isString[${inside}]`, result)
+    code = code.replaceLast(`$isString[${inside}]`, isNaN(inside))
 
     return {
         code: code
