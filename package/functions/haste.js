85285166d206c55e8498a15da6c36c93f2e52ea1
Speshultee <61945242+Speshultee@users.noreply.github.com>
2021-07-16 03:21:17 -0500

    $haste -Throws error if hastebin-gen not installed

--- package/functions/haste.js
+++ package/functions/haste.js
@@ -1,29 +1,34 @@
-const hastebin = require("hastebin-gen")
-const {docs} = require("../functions/docs/docs.json");
+const { docs } = require("../functions/docs/docs.json");
 
 const haste = async (client, message, args, name, code) => {
+    const hastebin;
+    try {
+        hastebin = require("hastebin-gen")
+    } catch {
+        return message.channel.send(`:x: Must \`npm install hastebin-gen\` before using \`$haste\`! \`$haste[${inside}]\`.\n${docs.action}/haste`)
+    }
 
-let r = code.split("$haste[").length - 1
+    let r = code.split("$haste[").length - 1
 
-let inside = code.split("$haste[")[r].split("]")[0]
+    let inside = code.split("$haste[")[r].split("]")[0]
 
-let fields = inside.split(";")
+    let fields = inside.split(";")
 
-if (!fields[0]) return message.channel.send(`:x: Empty content in \`$haste[${inside}]\`.\n${docs.action}/haste`)
+    if (!fields[0]) return message.channel.send(`:x: Empty content in \`$haste[${inside}]\`.\n${docs.action}/haste`)
 
-let content = fields[0]
+    let content = fields[0]
 
-let ext = fields[1]
+    let ext = fields[1]
 
-if (!fields[1]) ext = "txt"
+    if (!fields[1]) ext = "txt"
 
-let haste;
+    let haste;
 
-try {
-  haste = await hastebin(content, { extension: ext })
-} catch {
-  return message.channel.send(`Failed to create haste. Hastebin servers seem to be having issues.`)
-}
+    try {
+        haste = await hastebin(content, { extension: ext })
+    } catch {
+        return message.channel.send(`Failed to create haste. Hastebin servers seem to be having issues.`)
+    }
 
     code = code.replaceLast(`$haste[${inside}]`, haste)
 
